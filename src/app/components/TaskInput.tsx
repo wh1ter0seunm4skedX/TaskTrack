7ff93b93589bd17a7c6577cb582d4bd9f7bbfe11
Michael Bistritzki <101125385+wh1ter0seunm4skedX@users.noreply.github.com>
2024-10-19 10:50:01 +0300

    fix: added missing titles for the input adding task

--- src/app/components/TaskInput.tsx
+++ src/app/components/TaskInput.tsx
@@ -14,19 +14,17 @@ const TaskInput = ({ onAddTask, translations }: { onAddTask: (title: string, des
 
     return (
         <div className="p-4 bg-white shadow-md rounded-lg transition-transform transform hover:scale-105">
-            <h2 className="text-2xl font-semibold text-center mb-4 text-blue-600">
-                {translations.addNewTask}
-            </h2>
+            <h2 className="text-2xl font-semibold text-center mb-4 text-blue-600">{translations.addTask}</h2>
             <input
                 type="text"
                 className="border border-blue-300 p-2 w-full mb-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-shadow duration-300"
-                placeholder={translations.enterTaskTitle}
+                placeholder={translations.taskTitle || "Task Title"}
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
             />
             <textarea
                 className="border border-blue-300 p-2 w-full mb-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-shadow duration-300"
-                placeholder={translations.enterTaskDescription}
+                placeholder={translations.taskDescription || "Task Description"}
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
             />
